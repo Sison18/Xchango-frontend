70034ffc69a018816005ccbc6cd73bbeebd5c758
Sison18 <christianmarkbsison@gmail.com>
2025-07-06 14:15:42 -1200

    First Commit

---
+++ file.js
@@ -0,0 +1,21 @@
+{
+  /*
+FlatList - provides item and index by default when you use renderItem
+         - <FlatList> also expects a unique key for each item. You handle this with the keyExtractor prop.
+  -item: the current item in the data array,
+  -index: the index of that item  
+  
+.map(...) - loops through that list, rendering each category button. 
+.map((item, index, array) - default in order pero pwedeng irename
+
+SCROLLVIEW with .map() is better for small number of items but with large dynamic list FLATLIST is better.
+  
+renderItem - always receives an object like { item, index } 
+  
+  
+  
+  
+  
+  
+*/
+}
